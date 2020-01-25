6e976a5384107ff7895ca7ebcd8ca5d223cb4e36
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-01-25 10:36:02 -0500

    Forcefully add command in helper dir

--- .run.cmd
+++ .run.cmd
@@ -3,13 +3,19 @@
 :: Currently stores the playlist for my downloads
 set playlist_url=PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX
 
+if [%1]==[add] (
+  if [%2]==[command] ( call :add_command %3 )
+)
 if [%1]==[list] ( call :list_ids )
 
 exit /b
 goto :eof
 
+:add_command
+if NOT [%1]==[] ( git add %1 -f)
+goto :eof
+
 :list_ids
-:: call _\list-playlist.cmd %playlist_url%
 FOR /F "usebackq delims==" %%i IN (`_\list-playlist.cmd %playlist_url%`) DO ( @echo %%i )
 
 goto :eof
