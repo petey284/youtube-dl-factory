db487c4ae0cb4bd92c1c51580dc227cae9780977
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-01-25 10:30:07 -0500

    Setup for listing and printing in one step

--- .run.cmd
+++ .run.cmd
@@ -3,14 +3,13 @@
 :: Currently stores the playlist for my downloads
 set playlist_url=PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX
 
-if [%1]==[list] (
-  call :list_ids
-)
+if [%1]==[list] ( call :list_ids )
 
 exit /b
 goto :eof
 
 :list_ids
-call _\list-playlist.cmd %playlist_url%
+:: call _\list-playlist.cmd %playlist_url%
+FOR /F "usebackq delims==" %%i IN (`_\list-playlist.cmd %playlist_url%`) DO ( @echo %%i )
 
 goto :eof
