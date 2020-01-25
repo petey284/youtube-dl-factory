1af47efc494f0875b194615226313e970f10a97f
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-01-25 10:18:11 -0500

    update run command

--- .run.cmd
+++ .run.cmd
@@ -6,10 +6,11 @@ set playlist_url=PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX
 if [%1]==[list] (
   call :list_ids
 )
+
+exit /b
 goto :eof
 
 :list_ids
-:: youtube-dl -j --flat-playlist PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX | jq -r ".id" | sed 's_^_https://youtu.be/_' > batch-file.txt
-:: youtube-dl -j --flat-playlist PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX | jq -r ".id" | sed 's_^_https://youtu.be/_'
-youtube-dl -j --flat-playlist PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX | jq -r ".id"
+call _\list-playlist.cmd %playlist_url%
+
 goto :eof
